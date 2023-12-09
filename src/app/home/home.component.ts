b3bd85bdbc4804b0587215fc5023409773b07be1
thuthuy1901 <huyetlinh1901@gmail.com>
2023-12-09 22:45:10 +0700

    DataBinding-ngIf

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
   public name: string = 'Thuy';
-
-  constructor() {}
-
-  ngOnInit(): void {}
+  public age: number = 25;
 }
