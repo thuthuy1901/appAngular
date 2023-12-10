a23dce0605f1eece515b299eceeaad2cedc84f04
thuthuy1901 <huyetlinh1901@gmail.com>
2023-12-10 10:07:03 +0700

    ngClass

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   public name: string = 'Thuy';
   public age: number = 18;
+  public traiCay: string[] = ['táo', 'nho'];
+  public traiCay2: { name: string; price: number; sale: boolean }[] = [
+    { name: 'táo', price: 12, sale: true },
+    { name: 'nho', price: -12, sale: false },
+    { name: 'táo', price: 12, sale: true },
+    { name: 'nho', price: -12, sale: false },
+  ];
+
+  public ngOnInit(): void {
+    console.log(this.traiCay);
+  }
 }
