8c814bb6ab7ade144a8fba5301db150d801772f8
thuthuy1901 <huyetlinh1901@gmail.com>
2023-12-10 22:11:51 +0700

    ngSwitch

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrl: './about.component.scss'
+  styleUrl: './about.component.scss',
 })
 export class AboutComponent {
-
+  public loginName: string = 'admin';
 }
