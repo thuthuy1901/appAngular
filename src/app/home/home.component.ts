fc20ca83f0aa9fa587037991074379df310fd543
thuthuy1901 <huyetlinh1901@gmail.com>
2023-12-10 20:28:30 +0700

    Cascading ComboBox

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -15,8 +15,52 @@ export class HomeComponent implements OnInit {
     { name: 'táo', price: 12000000, sale: true },
     { name: 'nho', price: -10002, sale: false },
   ];
+  public VN: {
+    city: string;
+    district: string[];
+  }[] = [
+    { city: '--Select--', district: [] },
+    {
+      city: 'Hồ Chí Minh',
+      district: [
+        'Phường 1 - HCM',
+        'Phường 2 - HCM',
+        'Phường 3 - HCM',
+        'Phường A - HCM',
+        'Phường B - HCM',
+        'Phường C - HCM',
+      ],
+    },
+    {
+      city: 'Hà Nội',
+      district: [
+        'Phường 1 - HN',
+        'Phường 2 - HN',
+        'Phường 3 - HN',
+        'Phường A - HN',
+        'Phường B - HN',
+        'Phường C - HN',
+      ],
+    },
+  ];
+
+  public districts: string[] = [];
+
+  public changeCity(event: any): void {
+    const city = event.target.value;
+    if (!city) return;
+
+    // C1
+    // const search = this.VN.filter((data) => data.city === city);
+    // if (search && search.length > 0) {
+    //   this.districts = search[0].district;
+    // }
+
+    // C2
+    this.districts = this.VN.find((data) => data.city == city)?.district || [];
+  }
 
   public ngOnInit(): void {
-    console.log(this.traiCay);
+    console.log(this.VN);
   }
 }
